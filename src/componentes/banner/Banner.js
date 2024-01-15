85ee3d796309f191277cc6d4b34a0fb780d5763e
Daniel Amaral <Daniel.amaral1928@outlook.com>
2024-01-15 12:37:28 -0300

    creating main form

---
+++ src/componentes/banner/Banner.js
@@ -0,0 +1,14 @@
+import "./Banner.css"
+
+function Banner () {
+    //JSX
+    return(
+        <header className="banner">
+        <img src="/imagens/banner.png" alt="banner"></img>
+        </header>
+
+    )    
+
+}
+
+export default Banner
